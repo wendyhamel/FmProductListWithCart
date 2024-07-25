0ea96220a0dbd7895518974fc7ac832e0b7d2ed7
wendyhamel <whamel@it4web.nl>
2024-07-25 16:11:35 +0200

    Started setup with tailwind, alpine, colors and font

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['Red\\ Hat\\ Text', 'sans-serif']
+    },
+    extend: {
+      colors:
+          {
+            Red: 'hsl(14, 86%, 42%)',
+            Green: 'hsl(159, 69%, 38%)',
+            Rose: {
+              50 : 'hsl(20, 50%, 98%)',
+              100 : 'hsl(13, 31%, 94%)',
+              300 : 'hsl(14, 25%, 72%)',
+              400 : 'hsl(7, 20%, 60%)',
+              500 : 'hsl(12, 20%, 44%)',
+              900 : 'hsl(14, 65%, 9%)',
+
+            },
+          }
+    },
+  },
+  plugins: [],
+}
+
